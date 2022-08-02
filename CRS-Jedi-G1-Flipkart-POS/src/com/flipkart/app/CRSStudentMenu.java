e37c52b57bfe481a9001c80e162c0f86307f62d1
Kaustuv Banerjee <kaustuv.banerjee@BLRETV-T7D97W0H.local>
2022-08-02 10:40:36 +0530

    Added student registry for courses

--- CRS-Jedi-G1-Flipkart-POS/src/com/flipkart/app/CRSStudentMenu.java
+++ CRS-Jedi-G1-Flipkart-POS/src/com/flipkart/app/CRSStudentMenu.java
@@ -17,37 +17,40 @@ public class CRSStudentMenu {
 		// TODO Auto-generated method stub
 		System.out.println("Choose an option:-");
 		System.out.println("----------------------------------------------------------");
-		System.out.println("\t1 : View Course List\n\t2 : Payment\n\t3 : View Grade Card\n\t4 : View Profile\n\t5 : View Notifications\n\t6 : Logout");
+		System.out.println("\t1 : View Course List\n\t2 : Register for Courses\n\t3 : Payment\n\t4 : View Grade Card\n\t5 : View Profile\n\t6 : View Notifications\n\t7 : Logout");
 		System.out.println("==========================================================");
 		Scanner scanner = new Scanner(System.in);
 		StudentImpl studentImpl = new StudentImpl();
 		while(true) {
 			System.out.print("Enter your Choice: ");
 			int choice = scanner.nextInt();
-			if(choice == 6)
+			if(choice == 7)
 			{
 				CRSApplication.startApplication();
 				break;
 			}
 			switch(choice) {
 			case 1:{
-				System.out.println("View Course List");
 				studentImpl.displayCourseCatalog();
 				break;
 			}
 			case 2:{
-				System.out.println("Payment");
+				studentImpl.selectCourses(studentId);
 				break;
 			}
 			case 3:{
-				studentImpl.displayGradeCard(studentId);
+				System.out.println("Payment");
 				break;
 			}
 			case 4:{
-				System.out.println(studentImpl.viewStudentDetails(studentId).toString());
+				studentImpl.displayGradeCard(studentId);
 				break;
 			}
 			case 5:{
+				System.out.println(studentImpl.viewStudentDetails(studentId).toString());
+				break;
+			}
+			case 6:{
 				studentImpl.viewNotifications(studentId);
 				break;
 			}
--- CRS-Jedi-G1-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
+++ CRS-Jedi-G1-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
@@ -3,9 +3,11 @@ package com.flipkart.service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.flipkart.bean.*;
 import com.flipkart.dao.*;
+import jdk.internal.util.xml.impl.Pair;
 
 public class StudentImpl implements StudentInterface {
 	
@@ -49,24 +51,25 @@ public class StudentImpl implements StudentInterface {
 	@Override
 	public StudentCourseChoice selectCourses(String studentId) {
 		
-		ArrayList<Course> selectedCourses = new ArrayList<Course>();
-		ArrayList<Course> courseCatalog = catalogDaoImplementation.getAllCourses();
-		Map<Integer,Course> courseList = new HashMap<>();
+		ArrayList<Course> selectedCourses = new ArrayList<Pair<String, Integer>>();
+		ArrayList<Course> courseCatalog = catalogDaoImplementation.viewCatalog(2);
+		Map<String,Course> courseList = new HashMap<>();
 		for(Course c: courseCatalog)
 			courseList.put(c.getCourseId(), c);
 		for(int i=0; i<6; )
 		{
-			//System.out.print("Enter course(courseId) choice-"+i+": ");
-			//int courseId = sc.nextInt();
-			
-			int courseId = id.get(i);
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter course(courseId) choice-"+i+": ");
+			String courseId = sc.next();
+			System.out.print("Enter the type of choice (1-Core 2-Elective) : ");
+			Integer type = sc.nextInt();
 			Course course = null;
 			if(courseList.containsKey(courseId))
 				course = courseList.get(courseId);
 			if(course != null)
 			{
 				i++;
-				selectedCourses.add(course);
+				selectedCourses.add(make_pair(courseId,type));
 			}
 			else
 			{
@@ -81,16 +84,7 @@ public class StudentImpl implements StudentInterface {
 		studentDaoImplementation.storeStudentCourseChoice(studentCourseChoice);
 		
 		System.out.println("Registration form submitted!!");
-//		Notification notification = new Notification();
-//		notification.setUserId(CRSApplication.getUserId());
-//		notification.setMessage("Your application form has been submitted for the further process.");
-//		Date date = new Date();
-//		notification.setDateTime(date);
-//		AdminDaoInterface adminDao = new AdminDaoImpl();
-//		adminDao.generateNotification(notification);
 		return studentCourseChoice;
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
